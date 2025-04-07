fabbc937abfc440092c3323fe2841db2141c6e1d
Misael Taperia <askedmisael2@gmail.com>
2025-04-07 14:24:58 -0500

    saving changes

--- src/components/ProjectsLR.jsx
+++ src/components/ProjectsLR.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Project({ description, image }) {
   return (
-    <div className="Projects-Section">
+    <div className="Projects-SectionLR">
       <div className="Projects-desc">
         <p>{description}</p>
       </div>
